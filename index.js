cd200329acc23f0a8ebaff0a323a878d6c7846d0
Joachim Viide <jviide@iki.fi>
2019-12-13 20:52:31 +0200

    Add umount handling

--- index.js
+++ index.js
@@ -3,20 +3,29 @@ import { Component, createElement } from "preact";
 export const flowponent = flow => {
   return function(props, state) {
     if (!state.trigger) {
-      const iter = flow(props);
+      let iter = flow(props);
 
       const step = sentValue => {
-        new Promise(trigger => {
-          const next = iter.next(sentValue);
-          if (!next.done) {
-            this.setState({
-              view: next.value(trigger),
-              trigger: trigger
-            });
-          } else if (props.onReturn) {
-            props.onReturn(next.value);
-          }
-        }).then(step);
+        if (iter) {
+          new Promise(trigger => {
+            const next = iter.next(sentValue);
+            if (!next.done) {
+              this.setState({
+                view: next.value(trigger),
+                trigger: trigger
+              });
+            } else if (props.onReturn) {
+              props.onReturn(next.value);
+            }
+          }).then(step);
+        }
+      };
+
+      this.componentWillUnmount = () => {
+        if (iter) {
+          iter.return();
+          iter = null;
+        }
       };
 
       step();
